f82fe44d07b79550258ec1f733f77ef4e20dce24
Sebas Adrover <sebastia.adrover@melia.com>
2023-01-10 22:36:29 +0100

    Build: configure database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+CREATE TABLE SuperHeroes (HeroID int, HeroName varchar(255), FirstName varchar(255), SecondName varchar(255), Power varchar(255));
+
+INSERT INTO SuperHeroes (HeroID, HeroName, FirstName, SecondName, Power) VALUES (1, 'AllMight', 'Toshinori', 'Yagi', 'OneForAll');
+INSERT INTO SuperHeroes (HeroID, HeroName, FirstName, SecondName, Power) VALUES (2, 'Deku', 'Izuku', 'Midoriya', 'OneForAll');
+INSERT INTO SuperHeroes (HeroID, HeroName, FirstName, SecondName, Power) VALUES (3, 'Endeavor', 'Enji', 'Todoroky', 'Fire');
+INSERT INTO SuperHeroes (HeroID, HeroName, FirstName, SecondName, Power) VALUES (4, 'Kacchan Dynamight', 'Katsuki', 'Bakugo', 'Explosions');
