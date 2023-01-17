c26c66d77509278a02849d9e68d8464beb2be6b9
Sebas Adrover <sebastia.adrover@melia.com>
2023-01-17 23:10:51 +0100

    Refactor: remove unnecessary annotation

--- src/main/java/com/example/pruebatecnica/dtos/SuperHeroDto.java
+++ src/main/java/com/example/pruebatecnica/dtos/SuperHeroDto.java
@@ -1,11 +1,9 @@
 package com.example.pruebatecnica.dtos;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
-@AllArgsConstructor
 @Getter
 public class SuperHeroDto {
 
